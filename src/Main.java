1c5990388ac1dba2137e0a11dfd78096e1eee494
Nikita Romanovskiy <66296571+NikitaRomanof@users.noreply.github.com>
2023-03-25 16:47:32 +0700

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+       Map<Integer, Integer> test2= new Map<>();
+       test2.insert(5, 1);
+       test2.insert(8, 2);
+       test2.insert(9, 3);
+       test2.insert(6, 4);
+       test2.insert(3, 5);
+       test2.insert(2, 6);
+        test2.insert(1, 7);
+        test2.insert(0, 8);
+        test2.erase(3);
+       test2.printTree(null, "");
+
+
+
+
+
+
+
+
+
+    }
+
+}
